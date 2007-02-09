2aae208ab9811487514d1e278c88719aafa4664f
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2007-02-09 12:05:00 +0000

    MfcPort

--- winx/include/winx/ext/CreditStatic.h
+++ winx/include/winx/ext/CreditStatic.h
@@ -19,8 +19,8 @@
 #ifndef __WINX_EXT_CREDITSTATIC_H__
 #define __WINX_EXT_CREDITSTATIC_H__
 
-#ifndef __MFCPORT_AFX_H__
-#include "../../mfcport/afx.h"
+#ifndef __WINX_MFCPORT_H__
+#include "../MfcPort.h"
 #endif
 
 namespace winx {	//@@code insert
