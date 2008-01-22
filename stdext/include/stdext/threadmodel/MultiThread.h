95a4a3bcf64bef8b48c08e263cceb80dffd8eebc
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2008-01-22 14:14:12 +0000

    ThreadModel

---
+++ stdext/include/stdext/threadmodel/MultiThread.h
@@ -0,0 +1,87 @@
+/* -------------------------------------------------------------------------
+// WINX: a C++ template GUI library - MOST SIMPLE BUT EFFECTIVE
+// 
+// This file is a part of the WINX Library.
+// The use and distribution terms for this software are covered by the
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.txt at this distribution. By using
+// this software in any fashion, you are agreeing to be bound by the terms
+// of this license. You must not remove this notice, or any other, from
+// this software.
+// 
+// Module: stdext/threadmodel/MultiThread.h
+// Creator: xushiwei
+// Email: xushiweizh@gmail.com
+// Date: 2006-9-3 2:18:33
+// 
+// $Id: MultiThread.h,v 1.1 2006/10/18 12:13:39 xushiwei Exp $
+// -----------------------------------------------------------------------*/
+#ifndef __STDEXT_THREADMODEL_MULTITHREAD_H__
+#define __STDEXT_THREADMODEL_MULTITHREAD_H__
+
+// -------------------------------------------------------------------------
+// class RefCountMT
+
+class RefCountMT
+{
+public:
+	typedef LONG value_type;
+
+private:
+	value_type m_nRef;
+
+public:
+	RefCountMT(value_type nRef)
+		: m_nRef(nRef)
+	{
+	}
+
+	value_type winx_call inc()
+	{
+		return InterlockedIncrement(&m_nRef);
+	}
+
+	value_type winx_call dec()
+	{
+		return InterlockedDecrement(&m_nRef);
+	}
+
+	operator value_type()
+	{
+		return m_nRef;
+	}
+};
+
+// -------------------------------------------------------------------------
+// class CriticalSectionMT
+
+class CriticalSectionMT
+{
+private:
+	CRITICAL_SECTION m_cs;
+	
+public:
+	CriticalSectionMT()
+	{
+		InitializeCriticalSection(&m_cs);
+	}
+	~CriticalSectionMT()
+	{
+		DeleteCriticalSection(&m_cs);
+	}
+
+	void winx_call lock()
+	{
+		EnterCriticalSection(&m_cs);
+	}
+
+	void winx_call unlock()
+	{
+		LeaveCriticalSection(&m_cs);
+	}
+};
+
+// -------------------------------------------------------------------------
+//	$Log: $
+
+#endif /* __STDEXT_THREADMODEL_MULTITHREAD_H__ */
---
+++ stdext/include/stdext/threadmodel/SingleThread.h
@@ -0,0 +1,67 @@
+/* -------------------------------------------------------------------------
+// WINX: a C++ template GUI library - MOST SIMPLE BUT EFFECTIVE
+// 
+// This file is a part of the WINX Library.
+// The use and distribution terms for this software are covered by the
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.txt at this distribution. By using
+// this software in any fashion, you are agreeing to be bound by the terms
+// of this license. You must not remove this notice, or any other, from
+// this software.
+// 
+// Module: stdext/threadmodel/SingleThread.h
+// Creator: xushiwei
+// Email: xushiweizh@gmail.com
+// Date: 2006-9-3 2:18:33
+// 
+// $Id: SingleThread.h,v 1.1 2006/10/18 12:13:39 xushiwei Exp $
+// -----------------------------------------------------------------------*/
+#ifndef __STDEXT_THREADMODEL_SINGLETHREAD_H__
+#define __STDEXT_THREADMODEL_SINGLETHREAD_H__
+
+// -------------------------------------------------------------------------
+// struct RefCountST
+
+class RefCountST
+{
+public:
+	typedef long value_type;
+
+private:
+	value_type m_nRef;
+
+public:
+	RefCountST(value_type nRef) : m_nRef(nRef)
+	{
+	}
+
+	value_type winx_call inc()
+	{
+		return ++m_nRef;
+	}
+
+	value_type winx_call dec()
+	{
+		return --m_nRef;
+	}
+
+	operator value_type()
+	{
+		return m_nRef;
+	}
+};
+
+// -------------------------------------------------------------------------
+// class CriticalSectionST
+
+class CriticalSectionST
+{
+public:
+	void winx_call lock()	{}
+	void winx_call unlock() {}
+};
+
+// -------------------------------------------------------------------------
+//	$Log: $
+
+#endif /* __STDEXT_THREADMODEL_SINGLETHREAD_H__ */
