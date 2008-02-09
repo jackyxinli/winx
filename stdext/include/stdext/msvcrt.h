25228f6b3ae613c4ca27e2fdeb022843b682686c
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2008-02-09 14:59:00 +0000

    #9: Implement missing functions such as _itoa/_gcvt/_alloca/_msize

--- stdext/include/stdext/msvcrt.h
+++ stdext/include/stdext/msvcrt.h
@@ -24,56 +24,6 @@
 #endif
 
 // -------------------------------------------------------------------------
-// Visual C++ special
-
-#if defined(STD_NO_MSVCRT)
-
-//
-// stdio.h
-//
-#ifdef WINX_GCC
-#ifndef _STDIO_H
-#include <stdio.h>
-#endif
-#define _vsnprintf(buf, n, fmt, arglist) vsnprintf(buf, n, fmt, arglist)
-#endif
-
-//
-// io.h
-//
-#include <sys/stat.h>
-inline long _filelength(int fd)
-{
-    struct stat s;
-    if (fstat(fd, &s) < 0)
-        return 0;
-    return s.st_size;
-}
-
-//
-// assert.h
-//
-#ifdef WINX_GCC
-#ifndef _ASSERT_H
-#include <assert.h>
-#endif
-#define _assert(exp, file, line) __assert_fail(exp, (const char*)(file), line, __ASSERT_FUNCTION)
-#endif
-
-/*
-_itoa
-_ltoa
-_ultoa
-_i64toa
-_ui64toa
-_gcvt
-_alloca
-_msize
-*/
-
-#endif
-
-// -------------------------------------------------------------------------
 // ==== crtdbg.h ====
 
 #if (0)
@@ -101,6 +51,98 @@ _msize
 #include "msvcrt/crtdbg.h"
 #endif
 #endif // !defined(STD_NO_MSVCRT)
+
+// -------------------------------------------------------------------------
+// Visual C++ Special
+
+//
+// stdio.h
+//
+#ifdef WINX_GCC
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
+#define _vsnprintf(buf, n, fmt, arglist) vsnprintf(buf, n, fmt, arglist)
+#endif
+
+//
+// io.h
+//
+#ifndef _SYS_STAT_H
+#include <sys/stat.h>
+#endif
+inline long _filelength(int fd)
+{
+    struct stat s;
+    if (fstat(fd, &s) < 0)
+        return 0;
+    return s.st_size;
+}
+
+//
+// assert.h
+//
+#ifdef WINX_GCC
+#ifndef _ASSERT_H
+#include <assert.h>
+#endif
+#define _assert(exp, file, line) __assert_fail(exp, (const char*)(file), line, __ASSERT_FUNCTION)
+#endif
+
+//
+// stdlib.h
+//
+
+#ifdef WINX_GCC
+
+#ifndef __STDEXT_TCHAR_H__
+#include "tchar.h"
+#endif
+
+#define _itoa		tchar::itoa
+#define _ltoa		tchar::ltoa
+#define _ultoa		tchar::ultoa
+#define	_i64toa		tchar::i64toa
+#define _ui64toa	tchar::ui64toa
+
+inline void __not_impl(const char* szFeature)
+{
+	_RPT1(_CRT_ERROR, "NotImplementFeature - %s\n", szFeature);
+	throw szFeature;
+}
+
+inline char* _gcvt(double value, int ndec, char* buf)
+{
+	__not_impl("_gcvt");
+	return buf;
+}
+
+#endif // WINX_GCC
+
+//
+// malloc.h
+//
+
+#ifdef WINX_GCC
+
+#ifndef _ALLOCA_H
+#include <alloca.h>
+#endif
+
+#define _alloca	alloca
+
+#ifndef _MALLOC_H
+#include <malloc.h>
+#endif
+
+#define _msize malloc_usable_size
+// NOTE: 
+//	malloc_usable_size() returns the number of bytes you actually can use in a previously 
+//	allocated memory block. This value may be more than the amount you originally requested,
+//	due to alignment and minimum size constraints. For example, if you allocate 30 bytes, 
+//	the usable size is actually 36.
+
+#endif // WINX_GCC
 
 // -------------------------------------------------------------------------
 // ==== std::exception ====
