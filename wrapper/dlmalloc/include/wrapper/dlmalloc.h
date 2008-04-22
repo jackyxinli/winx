092d2e38dd0317fbcdfbe57f5bae947797eb33d2
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2008-04-22 15:52:42 +0000

    wrapper/dlmalloc.h

--- wrapper/dlmalloc/include/wrapper/dlmalloc.h
+++ wrapper/dlmalloc/include/wrapper/dlmalloc.h
@@ -19,6 +19,23 @@
 #ifndef __WRAPPER_DLMALLOC_H__
 #define __WRAPPER_DLMALLOC_H__
 
+#ifndef MALLINFO_FIELD_TYPE
+#define MALLINFO_FIELD_TYPE size_t
+#endif  /* MALLINFO_FIELD_TYPE */
+
+struct mallinfo {
+  MALLINFO_FIELD_TYPE arena;    /* non-mmapped space allocated from system */
+  MALLINFO_FIELD_TYPE ordblks;  /* number of free chunks */
+  MALLINFO_FIELD_TYPE smblks;   /* always 0 */
+  MALLINFO_FIELD_TYPE hblks;    /* always 0 */
+  MALLINFO_FIELD_TYPE hblkhd;   /* space in mmapped regions */
+  MALLINFO_FIELD_TYPE usmblks;  /* maximum total allocated space */
+  MALLINFO_FIELD_TYPE fsmblks;  /* always 0 */
+  MALLINFO_FIELD_TYPE uordblks; /* total allocated space */
+  MALLINFO_FIELD_TYPE fordblks; /* total free space */
+  MALLINFO_FIELD_TYPE keepcost; /* releasable (via malloc_trim) space */
+};
+
 /* ------------------- Declarations of public routines ------------------- */
 
 /*
