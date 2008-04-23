ab984aa3ddbca755bb0a9c9e25d30ab4fcaeb9a1
xushiweizh <xushiweizh@gmail.com>
2008-04-23 08:31:07 +0000

    issue #52: wrapper Boehm GC

---
+++ wrapper/gc/include/wrapper/gc.h
@@ -0,0 +1,32 @@
+/* -------------------------------------------------------------------------
+// WINX: a C++ template GUI library - MOST SIMPLE BUT EFFECTIVE
+// 
+// This file is a part of the WINX Library.
+// The use and distribution terms for this software are covered by the
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.txt at this distribution. By using
+// this software in any fashion, you are agreeing to be bound by the terms
+// of this license. You must not remove this notice, or any other, from
+// this software.
+// 
+// Module: wrapper/gc.h
+// Creator: xushiwei
+// Email: xushiweizh@gmail.com
+// Date: 2003-10-14 17:57:09
+// 
+// $Id: gc.h,v 1.1 2006/11/30 08:45:42 xushiwei Exp $
+// -----------------------------------------------------------------------*/
+#ifndef __WRAPPER_GC_H__
+#define __WRAPPER_GC_H__
+
+// -------------------------------------------------------------------------
+
+#ifndef _GC_H
+#include "gc/gc.h"
+#endif
+
+// -------------------------------------------------------------------------
+// $Log: gc.h,v $
+
+#endif /* __WRAPPER_GC_H__ */
+
