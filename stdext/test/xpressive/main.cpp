7dc57549ab7fc7d23e84ff635f96d6abd1074aaa
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2008-05-20 18:57:46 +0000

    1

--- stdext/test/xpressive/main.cpp
+++ stdext/test/xpressive/main.cpp
@@ -7,18 +7,21 @@
 
 #include "stdafx.h"
 
+boost::proto::terminal<char>::type const _ch_o = {'>'};
+
 inline void example()
 {
-	std::string str = "I was born on 5/30/1973 at 7am.\nI was born on 6/20/1975 at 7am.\n";
+	std::string str = "I was born on >5/30/1973 at 7am.\nI was born on 6/20/1975 at 7am.\n";
 
     // define some custom mark_tags with names more meaningful than s1, s2, etc.
     mark_tag day(1), month(2), year(3), delim(4);
 
     // this regex finds a date
-    regex_ date = (month= repeat<1,2>(_d))           // find the month ...
-               >> (delim= (set= '/','-'))            // followed by a delimiter ...
-               >> (day=   repeat<1,2>(_d)) >> delim  // and a day followed by the same delimiter ...
-               >> (year=  repeat<1,2>(_d >> _d));    // and the year.
+    regex_ date = *~_ch_o
+			>> (month= repeat<1,2>(_d))           // find the month ...
+            >> (delim= (set= '/','-'))            // followed by a delimiter ...
+            >> (day=   repeat<1,2>(_d)) >> delim  // and a day followed by the same delimiter ...
+            >> (year=  repeat<1,2>(_d >> _d));    // and the year.
 
 	regex_iterator_ cur( str.begin(), str.end(), date);
 	regex_iterator_ end;
@@ -27,6 +30,7 @@ inline void example()
 	{
 		match_results_ item = *cur;
 		sub_match_ matched = item[0];
+		std::cout << matched << '\n';
 		std::cout << item[year] << '-' << item[month] << '-' << item[day] << '\n';
 	}
 }
