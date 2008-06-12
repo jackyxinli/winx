459bfcc57ba7b2a540e30ebd18dcd560a1c8a0fa
xushiweizh <xushiweizh@gmail.com>
2008-06-12 17:59:40 +0000

    issue #68: 1. create tpl Emulator library (tpl::emu) 2. tpl/emu/Function.h 3. tpl/emu/Instruction.h

---
+++ tpl/include/tpl/emu/Instruction.h
@@ -0,0 +1,403 @@
+/* -------------------------------------------------------------------------
+// WINX: a C++ template GUI library - MOST SIMPLE BUT EFFECTIVE
+// 
+// This file is a part of the WINX Library.
+// The use and distribution terms for this software are covered by the
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.txt at this distribution. By using
+// this software in any fashion, you are agreeing to be bound by the terms
+// of this license. You must not remove this notice, or any other, from
+// this software.
+// 
+// Module: tpl/emu/Instruction.h
+// Creator: xushiwei
+// Email: xushiweizh@gmail.com
+// Date: 2006-8-13 9:41:58
+// 
+// $Id: Instruction.h 588 2008-05-28 07:22:04Z xushiweizh $
+// -----------------------------------------------------------------------*/
+#ifndef TPL_ENU_INSTRUCTION_H
+#define TPL_ENU_INSTRUCTION_H
+
+#ifndef TPL_EMU_FUNCTION_H
+#include "Function.h"
+#endif
+
+NS_TPL_ENU_BEGIN
+
+// =========================================================================
+// Instruction
+
+union Operand
+{
+	int ival;
+	size_t val;
+	void* ptr;
+};
+
+template <class StackT, class ExecuteContextT>
+class Instruction
+{
+private:
+	typedef void (*Op)(Operand para, StackT& stk, ExecuteContextT& context);
+
+public:
+	const Op op;
+	Operand para;
+
+public:
+	Instruction(Op op_) : op(op_) {}
+	Instruction(Op op_, int val_) : op(op_) 	{ para.ival = val_; }
+	Instruction(Op op_, size_t val_) : op(op_) 	{ para.val = val_; }
+	Instruction(Op op_, void* ptr_) : op(op_) 	{ para.ptr = ptr_; }
+};
+
+// =========================================================================
+// class OpInstr
+
+// Usage:
+//		add		; OpInstr<std::plus, StackT, ContextT>::instr()
+//		sub		; OpInstr<std::minus, StackT, ContextT>::instr()
+//		mul		; OpInstr<std::multiplies, StackT, ContextT>::instr()
+//		div		; OpInstr<std::divides, StackT, ContextT>::instr()
+
+template <template <class Type> class Op_, class StackT, class ExecuteContextT>
+class OpInstr
+{
+private:
+	typedef typename StackT::value_type Ty;
+	enum { n_arity = ArityTraits<Op_, Ty>::value };
+
+public:
+	static void op(Operand, StackT& stk, ExecuteContextT&) {
+		Function<Op_<Ty>, n_arity> fn_;
+		fn_(stk);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr() {
+		return Instruction<StackT, ExecuteContextT>(op);
+	}
+};
+
+// =========================================================================
+// class FnInstr
+
+// Usage:
+//		pow		; FnInstr<2, StackT, ContextT>::instr(pow)
+//		sin		; FnInstr<1, StackT, ContextT>::instr(sin)
+//		cos		; FnInstr<1, StackT, ContextT>::instr(cos)
+
+template <int nArity, class StackT, class ExecuteContextT>
+class FnInstr
+{
+private:
+	typedef typename StackT::value_type Ty; 
+	typedef typename OpTraits<nArity, Ty>::op_type Op;
+
+	static void op(StackT& stk, Operand para) {
+		Function<Op, nArity> fn_((Op)para.ptr);
+		fn_(stk);
+	}
+
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(Op fn) {
+		return Instruction<StackT, ExecuteContextT>(op, fn);
+	}
+};
+
+// =========================================================================
+// class Push
+
+// Usage: push <val>
+
+template <class StackT, class ExecuteContextT>
+class Push
+{
+private:
+	typedef typename StackT::value_type ValT;
+
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		stk.push_back(*(const ValT*)para.ptr);
+	}
+	
+	template <class AllocT>
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(AllocT& alloc, const ValT& val) {
+		const ValT* p = TPL_NEW(alloc, ValT)(val);
+		return Instruction<StackT, ExecuteContextT>(op, p);
+	}
+};
+
+// =========================================================================
+// class Jmp
+
+// Usage: jmp <delta>
+
+template <class StackT, class ExecuteContextT>
+class Jmp
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		context.jump(para.ival);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(int delta) {
+		return Instruction<StackT, ExecuteContextT>(op, delta);
+	}
+};
+
+// =========================================================================
+// class JmpIfFalse
+
+// Usage: je <delta>
+
+template <class StackT, class ExecuteContextT>
+class JmpIfFalse
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		const typename StackT::value_type val = stk.back();
+		stk.pop_back();
+		if (!val)
+			context.jump(para.ival);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(int delta) {
+		return Instruction<StackT, ExecuteContextT>(op, delta);
+	}
+};
+
+// =========================================================================
+// class Arity
+
+// Usage: arity <count>
+
+template <class StackT, class ExecuteContextT>
+class Arity
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT&) {
+		stk.push_back(para.val);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(size_t n) {
+		return Instruction<StackT, ExecuteContextT>(op, n);
+	}
+};
+
+// =========================================================================
+// class Local
+
+// Usage: local <count>
+
+template <class StackT, class ExecuteContextT>
+class Local
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT&) {
+		stk.resize(stk.size() + para.val);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(size_t n) {
+		return Instruction<StackT, ExecuteContextT>(op, n);
+	}
+};
+
+// =========================================================================
+// class CallerFrame
+
+// Function Stack Frame
+//	 arg1
+//	 arg2
+//	 ...
+//	 argn
+//	 [arity]
+//	 callerframe
+//	 ------------ <frame>
+//	 [local_var1]
+//	 [local_var2]
+//	 ...
+//	 [local_varn]
+//	 return_val
+
+class CallerFrame
+{
+public:
+	enum { SIZE = 2 };
+	
+	enum { ARITY = -3 };
+	enum { RETURN_IP = -2 };
+	enum { BP = -1 };
+	
+public:
+	template <class StackT, class ExecuteContextT>
+	static void TPL_CALL call(StackT& stk, ExecuteContextT& context, int delta) {
+		stk.push_back(context.position());
+		stk.push_back(context.frame());
+		context.frame(stk.size());
+		context.jump(delta);
+	}
+
+	template <class StackT, class ExecuteContextT>
+	static void TPL_CALL ret(StackT& stk, ExecuteContextT& context, size_t n) {
+		typename StackT::value_type val = stk.top();
+		size_t frame_ = context.frame();
+		context.frame(stk[frame_ + BP]);
+		context.position(stk[frame_ + RETURN_IP]);
+		stk.resize(frame_ - n);
+		stk.push_back(val);
+	}
+
+public:
+	template <class StackT, class ExecuteContextT>
+	size_t TPL_CALL arity(StackT& stk, ExecuteContextT& context) const {
+		const size_t arity_idx_ = context.frame() + ARITY;
+		return stk[arity_idx_];
+	}
+};
+
+// =========================================================================
+// class Call
+
+// Usage: call <delta>
+//
+// Example1:
+//	push 3.0
+//	push 2.0
+//	call offset pow
+// Meanings:
+//	pow(3.0, 2.0)
+//
+// Example2:
+//	push 3.0
+//	push 2.0
+//	push 1.0
+//	arity 3
+//	call offset max
+// Meanings:
+//	max(3.0, 2.0, 1.0)
+
+template <class StackT, class ExecuteContextT>
+class Call
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		CallerFrame::call(stk, context, para.ival);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(int delta) {
+		return Instruction<StackT, ExecuteContextT>(op, delta);
+	}
+};
+
+// =========================================================================
+// class LoadArg/LoadVArgs/LoadLocal
+
+// Usage: load_arg <offset>	; here <offset> can be -n ~ -1
+//	 arg1 = load_arg -n
+//	 arg2 = load_arg -(n-1)
+//	 ...
+//	 argn = load_arg -1
+
+template <class StackT, class ExecuteContextT>
+class LoadArg
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		stk.push_back(stk[context.frame() + para.ival]);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(int delta) {
+		return Instruction<StackT, ExecuteContextT>(op, delta - CallerFrame::SIZE);
+	}
+};
+
+// Usage: load_vargs
+
+template <class StackT, class ExecuteContextT>
+class LoadVArgs
+{
+public:	
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		typedef typename StackT::value_type ValT;
+		const size_t arity_ = CallerFrame::arity(stk, context);
+		stk.push_back(ValT::array(stk, arity_idx_ - arity_, arity_));
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr() {
+		return Instruction<StackT, ExecuteContextT>(op);
+	}
+};
+
+// Usage: load_local <index> ; here <index> can be 0 ~ n-1
+//	 local_var1 = load_local 0
+//	 local_var2 = load_local 1
+//	 ...
+//	 local_varn = load_local n-1
+
+template <class StackT, class ExecuteContextT>
+class LoadLocal
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		stk.push_back(stk[context.frame() + para.val]);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(size_t delta) {
+		return Instruction<StackT, ExecuteContextT>(op, delta);
+	}
+};
+
+// =========================================================================
+// class Ret/RetN
+
+// Usage: ret <n>
+//
+// Example1:
+//	 proc my_pow
+//		load_arg -2		; push arg1
+//		load_arg -1		; push arg2
+//		pow				; call pow instruction 
+//		ret 2			; return
+//	end proc
+//
+// Example2:
+//	 proc my_max
+//		load_vargs		; push arg[]
+//		max_n			; call max_n instruction
+//		ret				; return
+//	 end proc
+
+template <class StackT, class ExecuteContextT>
+class RetN
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		CallerFrame::ret(stk, context, para.val);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr(size_t n) {
+		return Instruction<StackT, ExecuteContextT>(op, n + CallerFrame::SIZE);
+	}
+};
+
+template <class StackT, class ExecuteContextT>
+class Ret
+{
+public:
+	static void op(Operand para, StackT& stk, ExecuteContextT& context) {
+		const size_t n = CallerFrame::arity(stk, context) + (CallerFrame::SIZE + 1);
+		CallerFrame::ret(stk, context, n);
+	}
+	
+	static Instruction<StackT, ExecuteContextT> TPL_CALL instr() {
+		return Instruction<StackT, ExecuteContextT>(op);
+	}
+};
+
+// =========================================================================
+// $Log: $
+
+NS_TPL_EMU_END
+
+#endif /* TPL_ENU_INSTRUCTION_H */
+
--- tpl/include/tpl/ext/Calculator.h
+++ tpl/include/tpl/ext/Calculator.h
@@ -23,156 +23,46 @@
 #include "../RegExp.h"
 #endif
 
-#if !defined(_FUNCTIONAL_) && !defined(_FUNCTIONAL)
-#include <functional>
+#ifndef TPL_EMU_FUNCTION_H
+#include "../emu/Function.h"
 #endif
 
-#pragma warning(disable:4819)
-
-#ifndef BOOST_TT_IS_CONVERTIBLE_HPP_INCLUDED
-#include <boost/type_traits/is_convertible.hpp>
+#if !defined(_FUNCTIONAL_) && !defined(_FUNCTIONAL)
+#include <functional>
 #endif
 
 NS_TPL_BEGIN
 
 // =========================================================================
-// class Caller
-
-template <int nArity>
-struct Caller {};
-
-template <>
-struct Caller<1> {
-	template <class Op, class ValT>
-	__forceinline static ValT TPL_CALL call(const Op& op_, const ValT args[]) {
-		return op_(args[0]);
-	}
-};
-
-template <>
-struct Caller<2> {
-	template <class Op, class ValT>
-	__forceinline static ValT TPL_CALL call(const Op& op_, const ValT args[]) {
-		return op_(args[0], args[1]);
-	}
-};
-
-template <>
-struct Caller<3> {
-	template <class Op, class ValT>
-	__forceinline static ValT TPL_CALL call(const Op& op_, const ValT args[]) {
-		return op_(args[0], args[1], args[2]);
-	}
-};
-
-template <>
-struct Caller<4> {
-	template <class Op, class ValT>
-	__forceinline static ValT TPL_CALL call(const Op& op_, const ValT args[]) {
-		return op_(args[0], args[1], args[2], args[3]);
-	}
-};
-
-template <>
-struct Caller<5> {
-	template <class Op, class ValT>
-	__forceinline static ValT TPL_CALL call(const Op& op_, const ValT args[]) {
-		return op_(args[0], args[1], args[2], args[3], args[4]);
-	}
-};
-
-template <>
-struct Caller<6> {
-	template <class Op, class ValT>
-	__forceinline static ValT TPL_CALL call(const Op& op_, const ValT args[]) {
-		return op_(args[0], args[1], args[2], args[3], args[4], args[5]);
-	}
-};
-
-// =========================================================================
 // class Function
 
 template <class Op, class StackT, int nArity>
-class Function
+class Function : public emu::Function<Op, nArity>
 {
 private:
-	typedef typename StackT::value_type value_type;
+	typedef emu::Function<Op, nArity> Base;
 
-private:
 	StackT& m_stk;
-	const Op m_op;
 
 public:
-	Function(StackT& stk) : m_stk(stk), m_op() {}
+	Function(StackT& stk) : m_stk(stk), Base() {}
 
 	template <class T1>
-	Function(StackT& stk, const T1& arg) : m_stk(stk), m_op(arg) {}
+	Function(StackT& stk, const T1& arg) : m_stk(stk), Base(arg) {}
 
 public:
 	void TPL_CALL operator()() const {
-		TPL_ASSERT(m_stk.size() >= nArity);
-		value_type args[nArity];
-		for (int i = nArity; i--;) {		
-			args[i] = m_stk.back();
-			m_stk.pop_back();
-		}
-		m_stk.push_back(Caller<nArity>::call(m_op, args));
-	}
-};
-
-// class Unary-Function
-
-template <class Op, class StackT>
-class Function<Op, StackT, 1>
-{
-private:
-	typedef typename StackT::value_type value_type;
-
-private:
-	StackT& m_stk;
-	const Op m_op;
-
-public:
-	Function(StackT& stk) : m_stk(stk), m_op() {}
-
-	template <class T1>
-	Function(StackT& stk, const T1& arg) : m_stk(stk), m_op(arg) {}
-
-public:
-	void TPL_CALL operator()() const {
-		TPL_ASSERT(m_stk.size() >= 1);
-		value_type x = m_stk.back();
-		m_stk.pop_back();
-		m_stk.push_back(m_op(x));
+		Base::operator()(m_stk);
 	}
 };
 
 // -------------------------------------------------------------------------
 // function calc/1-ary
 
-template <int isUnary, int isBinary, int isTernary = 0>
-struct ArityTratis_ {};
-
-template <>
-struct ArityTratis_<1, 0, 0> { enum { value = 1 }; };
-
-template <>
-struct ArityTratis_<0, 1, 0> { enum { value = 2 }; };
-
-template <>
-struct ArityTratis_<0, 0, 1> { enum { value = 3 }; };
-
 template <template <class Type> class Op_, class StackT>
-class CalcTraits_
-{
-private:
-	typedef typename StackT::value_type Ty;
-	typedef Op_<Ty> Op;
-	enum { is_unary = boost::is_convertible<Op, std::unary_function<Ty, Ty> >::value };
-	enum { is_binary = boost::is_convertible<Op, std::binary_function<Ty, Ty, Ty> >::value };
-	enum { n_arity = ArityTratis_<is_unary, is_binary>::value };
-
-public:
+struct CalcTraits_ {
+	enum { n_arity = emu::ArityTraits<Op_>::value };
+	typedef Op_<typename StackT::value_type> Op;
 	typedef Function<Op, StackT, n_arity> action_type;
 };
 
@@ -216,59 +106,14 @@ SimpleAction<CheckArity<nArity> > TPL_CALL check_arity(const int& nArgv) {
 // function calc/2-ary, calc/3-ary
 
 template <int nArity, class StackT>
-struct CalcTraits_N_ {};
-
-template <class StackT>
-struct CalcTraits_N_<1, StackT>
-{
+struct CalcTraits_N_ {
 	typedef typename StackT::value_type Ty;
-	typedef Ty (*Op)(Ty);
-	typedef Function<Op, StackT, 1> action_type;
-};
-
-template <class StackT>
-struct CalcTraits_N_<2, StackT>
-{
-	typedef typename StackT::value_type Ty;
-	typedef Ty (*Op)(Ty, Ty);
-	typedef Function<Op, StackT, 2> action_type;
-};
-
-template <class StackT>
-struct CalcTraits_N_<3, StackT>
-{
-	typedef typename StackT::value_type Ty;
-	typedef Ty (*Op)(Ty, Ty, Ty);
-	typedef Function<Op, StackT, 3> action_type;
-};
-
-template <class StackT>
-struct CalcTraits_N_<4, StackT>
-{
-	typedef typename StackT::value_type Ty;
-	typedef Ty (*Op)(Ty, Ty, Ty, Ty);
-	typedef Function<Op, StackT, 4> action_type;
-};
-
-template <class StackT>
-struct CalcTraits_N_<5, StackT>
-{
-	typedef typename StackT::value_type Ty;
-	typedef Ty (*Op)(Ty, Ty, Ty, Ty, Ty);
-	typedef Function<Op, StackT, 5> action_type;
-};
-
-template <class StackT>
-struct CalcTraits_N_<6, StackT>
-{
-	typedef typename StackT::value_type Ty;
-	typedef Ty (*Op)(Ty, Ty, Ty, Ty, Ty, Ty);
-	typedef Function<Op, StackT, 6> action_type;
+	typedef typename emu::OpTraits<nArity, Ty>::op_type Op;
+	typedef Function<Op, StackT, nArity> action_type;
 };
 
 template <int nArity, class StackT>
-struct CalcTraitsEx_
-{
+struct CalcTraitsEx_ {
 	typedef CalcTraits_N_<nArity, StackT> Tr_;
 	typedef CheckArity<nArity> Action1;
 	typedef typename Tr_::action_type Action2;
