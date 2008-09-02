db8fe6b7cad990a58e0a7d39def6971b8362e409
xushiweizh <xushiweizh@gmail.com>
2008-09-02 09:57:50 +0000

    validation: use boost phoenix

--- tpl/examples/validation/Simplest.cpp
+++ tpl/examples/validation/Simplest.cpp
@@ -17,11 +17,46 @@ void validation_example_1(const char* str)
 
 // -------------------------------------------------------------------------
 
+void validation_example_2(const char* str)
+{
+	int val;
+	if (str == integer()/assign(val) && val >= 1 && val <= 12)
+		std::cout << "  " << str << " is an integer value between 1 to 12.\n";
+	else
+		std::cout << "  " << str << " is not valid data.\n";
+}
+
+// -------------------------------------------------------------------------
+
+#include <boost/spirit/phoenix.hpp>
+
+using namespace phoenix;
+
+void validation_example_3(const char* str)
+{
+	if (str == integer()/meet(arg1 >= 1 && arg1 <= 12))
+		std::cout << "  " << str << " is an integer value between 1 to 12.\n";
+	else
+		std::cout << "  " << str << " is not valid data.\n";
+}
+
+// -------------------------------------------------------------------------
+
 int main()
 {
 	validation_example_1("-135");
 	validation_example_1("+.23e-23");
 	validation_example_1("-.1.e23");
+
+	validation_example_2("3");
+	validation_example_2("13");
+	validation_example_2("-135");
+	validation_example_2("+.23e-23");
+
+	validation_example_3("3");
+	validation_example_3("13");
+	validation_example_3("-135");
+	validation_example_3("+.23e-23");
 	return 0;
 }
 
