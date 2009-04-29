cb43887d156b324628186447623a304c66a13ce5
xushiweizh <xushiweizh@gmail.com>
2009-04-29 15:54:19 +0000

    cp stdext

--- winx/bin/install(zh-CN).bat
+++
@@ -1,13 +0,0 @@
-@echo off
-
-if "%MSDEVDIR%"=="" goto error
-if not exist "%MSDEVDIR%\Template" md "%MSDEVDIR%\Template" 
-echo on
-copy zh-CN\winxwiz60.awx "%MSDEVDIR%\Template"
-@pause
-exit
-
-
-:error
-echo ERROR: Environ var 'MSDEVDIR' is undefined. MAYBE you can set 'MSDEVDIR' to be 'C:\Program Files\Microsoft Visual Studio\Common\MSDev98'?
-pause
