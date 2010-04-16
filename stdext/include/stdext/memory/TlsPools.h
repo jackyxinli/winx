c3eb1583dd1e007f07e2ab24cd278103640ec484
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2010-04-16 10:47:54 +0000

    class tls_pools - todo;

---
+++ stdext/include/stdext/memory/TlsPools.h
@@ -0,0 +1,39 @@
+/* -------------------------------------------------------------------------
+// WINX: a C++ template GUI library - MOST SIMPLE BUT EFFECTIVE
+// 
+// This file is a part of the WINX Library.
+// The use and distribution terms for this software are covered by the
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.txt at this distribution. By using
+// this software in any fashion, you are agreeing to be bound by the terms
+// of this license. You must not remove this notice, or any other, from
+// this software.
+// 
+// Module: stdext/memory/TlsPools.h
+// Creator: xushiwei
+// Email: xushiweizh@gmail.com
+// Date: 2006-8-18 13:46:39
+// 
+// $Id: TlsPools.h,v 1.1 2006/10/18 12:13:39 xushiwei Exp $
+// -----------------------------------------------------------------------*/
+#ifndef STDEXT_MEMORY_TLSPOOLS_H
+#define STDEXT_MEMORY_TLSPOOLS_H
+
+#ifndef STDEXT_BOOST_MEMORY_H
+#include "../boost/Memory.h"
+#endif
+
+NS_BOOST_MEMORY_BEGIN
+
+// -------------------------------------------------------------------------
+// class tls_pools
+
+class tls_pools
+{
+};
+
+// -------------------------------------------------------------------------
+
+NS_BOOST_MEMORY_END
+
+#endif /* STDEXT_MEMORY_TLSPOOLS_H */
