0cde91d978f1a53c4718b4b55eac08b52e5da032
xushiweizh <xushiweizh@cd33a3e1-671d-0410-bc12-7f66f1ecff36>
2010-04-27 15:41:45 +0000

    formatter - support afterPrint event;

--- stdext/include/stdext/text/format/formatter.h
+++ stdext/include/stdext/text/format/formatter.h
@@ -26,9 +26,20 @@
 NS_STDEXT_BEGIN
 
 // -------------------------------------------------------------------------
+
+namespace format_detail
+{
+	struct NullAction
+	{
+		template <class StringT>
+		static void winx_call afterPrint(StringT& dest) {}
+	};
+}
+
+// -------------------------------------------------------------------------
 // class FormatAppender
 
-template <class StringT>
+template <class StringT, class ActionT = format_detail::NullAction>
 class FormatAppender
 {
 protected:
@@ -44,24 +55,28 @@ public:
 	void winx_call print(const CharT* fmt)
 	{
 		NS_STDEXT_TEXT::append(dest, fmt);
+		ActionT::afterPrint(dest);
 	}
 
 	template <class CharT, class ArgT1>
 	void winx_call print(const CharT* fmt, const ArgT1& arg1)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1);
+		ActionT::afterPrint(dest);
 	}
 
 	template <class CharT, class ArgT1, class ArgT2>
 	void winx_call print(const CharT* fmt, const ArgT1& arg1, const ArgT2& arg2)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2);
+		ActionT::afterPrint(dest);
 	}
 
 	template <class CharT, class ArgT1, class ArgT2, class ArgT3>
 	void winx_call print(const CharT* fmt, const ArgT1& arg1, const ArgT2& arg2, const ArgT3& arg3)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3);
+		ActionT::afterPrint(dest);
 	}
 
 	template <class CharT, class ArgT1, class ArgT2, class ArgT3, class ArgT4>
@@ -70,6 +85,7 @@ public:
 		const ArgT1& arg1, const ArgT2& arg2, const ArgT3& arg3, const ArgT4& arg4)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3, arg4);
+		ActionT::afterPrint(dest);
 	}
 
 	template <class CharT, class ArgT1, class ArgT2, class ArgT3, class ArgT4, class ArgT5>
@@ -79,6 +95,7 @@ public:
 		const ArgT3& arg3, const ArgT4& arg4, const ArgT5& arg5)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3, arg4, arg5);
+		ActionT::afterPrint(dest);
 	}
 
 	template <
@@ -90,6 +107,7 @@ public:
 		const ArgT3& arg3, const ArgT4& arg4, const ArgT5& arg5, const ArgT6& arg6)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3, arg4, arg5, arg6);
+		ActionT::afterPrint(dest);
 	}
 
 	template <
@@ -101,6 +119,7 @@ public:
 		const ArgT4& arg4, const ArgT5& arg5, const ArgT6& arg6, const ArgT7& arg7)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+		ActionT::afterPrint(dest);
 	}
 
 	template <
@@ -112,6 +131,7 @@ public:
 		const ArgT4& arg4, const ArgT5& arg5, const ArgT6& arg6, const ArgT7& arg7, const ArgT8& arg8)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+		ActionT::afterPrint(dest);
 	}
 
 	template <
@@ -123,6 +143,7 @@ public:
 		const ArgT5& arg5, const ArgT6& arg6, const ArgT7& arg7, const ArgT8& arg8, const ArgT9& arg9)
 	{
 		NS_STDEXT::formatAppend(dest, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+		ActionT::afterPrint(dest);
 	}
 };
 
@@ -133,10 +154,23 @@ inline FormatAppender<StringT> winx_call formatter(StringT& dest)
 	return FormatAppender<StringT>(dest);
 }
 
+template <class StringT, class AfterPrintT>
+inline FormatAppender<StringT> winx_call formatter(StringT& dest, AfterPrintT action)
+{
+	NS_STDEXT_TEXT::clear(dest);
+	return FormatAppender<StringT, AfterPrintT>(dest);
+}
+
 template <class StringT>
 inline FormatAppender<StringT> winx_call formatAppender(StringT& dest)
 {
 	return FormatAppender<StringT>(dest);
+}
+
+template <class StringT, class AfterPrintT>
+inline FormatAppender<StringT> winx_call formatAppender(StringT& dest, AfterPrintT action)
+{
+	return FormatAppender<StringT, AfterPrintT>(dest);
 }
 
 // -------------------------------------------------------------------------
